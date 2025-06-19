7746f3a9c3ee5cd20b5b3f9965263719b0dfa757
Christian Ken Castro <christiankenc@yahoo.com>
2025-06-18 22:55:51 -0600

    update:
    - finished login, logout endpoint
    
    - created function to generate and set cookie

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,49 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+
 export const login = async (req, res) => {
-  res.send("login api");
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect password" });
+    }
+
+    generateTokenAndSetCookie(res, user._id);
+
+    user.lastLogin = new Date();
+    await user.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.error("Error in login", error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
 };
 
 export const logout = async (req, res) => {
-  res.send("logout api");
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+  });
+  res.status(200).json({ success: true, message: "Logged out successfully" });
 };
